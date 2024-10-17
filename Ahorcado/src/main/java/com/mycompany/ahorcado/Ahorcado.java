c8a52654952434a7fe565bfd225d856bbce7172a
rpaternina <robertpaternina65@gmail.com>
2024-10-17 15:12:41 -0500

    Primer commit

---
+++ Ahorcado/src/main/java/com/mycompany/ahorcado/Ahorcado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ahorcado;
+
+/**
+ *
+ * @author 57300
+ */
+public class Ahorcado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
